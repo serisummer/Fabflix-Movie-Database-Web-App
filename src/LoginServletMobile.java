06353de3583a9b6f858fee2eed6768e5ec5e43b8
Weiguang Wu <wwg12121@gmail.com>
2023-11-26 17:07:46 -0800

    Added login servlet for mobile

---
+++ src/LoginServletMobile.java
@@ -0,0 +1,100 @@
+import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+
+@WebServlet(name = "LoginServletMobile", urlPatterns = "/api/login-mobile")
+public class LoginServletMobile extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    // Create a dataSource which registered in web.
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        request.getServletContext().log("login attempt by: " + username + " using password: " + password);
+        JsonObject responseJsonObject = new JsonObject();
+
+        try (Connection conn = dataSource.getConnection()) {
+            String query = "select * from customers where email like ?";
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, username);
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next()) {
+                String resPassword = rs.getString("password");
+                String id = rs.getString("id");
+                if (password.equals(resPassword)) {
+                    HttpSession session = request.getSession(true);
+
+                    if (session.getAttribute("user") == null) {
+                        session.setAttribute("user", new User(username, id));
+                    }
+                    // Retrieve data named "accessCount" from session, which count how many times the user requested before
+                    Integer accessCount = (Integer) session.getAttribute("accessCount");
+
+                    if (accessCount == null) {
+                        accessCount = 0;
+                    } else {
+                        accessCount++;
+                    }
+                    // Update the new accessCount to session, replacing the old value if existed
+                    session.setAttribute("accessCount", accessCount);
+
+                    responseJsonObject.addProperty("status", "success");
+                    responseJsonObject.addProperty("message", "success");
+
+                } else {
+                    responseJsonObject.addProperty("status", "fail");
+                    // Log to localhost log
+                    request.getServletContext().log("Login failed");
+                    responseJsonObject.addProperty("message", "incorrect password");
+                }
+            }
+            else {
+                responseJsonObject.addProperty("status", "fail");
+                // Log to localhost log
+                request.getServletContext().log("Login failed");
+                responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
+            }
+            out.write(responseJsonObject.toString());
+            rs.close();
+            statement.close();
+
+            // Set response status to 200 (OK)
+            response.setStatus(200);
+
+        } catch (Exception e) {
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+    }
+}
